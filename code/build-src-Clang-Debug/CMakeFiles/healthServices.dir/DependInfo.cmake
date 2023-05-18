3b98ddff9bbb12a9796430ac8e1187712d355b8d
vdavv <vdavvv@ya.ru>
2023-05-18 14:55:27 +0300

    no errors

---
+++ code/build-src-Clang-Debug/CMakeFiles/healthServices.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/vdav/Yandex.Disk.localized/HSE/Cpp/BigHW/dsba-itop2023-hw/code/build-src-Clang-Debug/healthServices_autogen/mocs_compilation.cpp" "CMakeFiles/healthServices.dir/healthServices_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/healthServices.dir/healthServices_autogen/mocs_compilation.cpp.o.d"
+  "/Users/vdav/Yandex.Disk.localized/HSE/Cpp/BigHW/dsba-itop2023-hw/code/src/main.cpp" "CMakeFiles/healthServices.dir/main.cpp.o" "gcc" "CMakeFiles/healthServices.dir/main.cpp.o.d"
+  "/Users/vdav/Yandex.Disk.localized/HSE/Cpp/BigHW/dsba-itop2023-hw/code/src/mainwindow.cpp" "CMakeFiles/healthServices.dir/mainwindow.cpp.o" "gcc" "CMakeFiles/healthServices.dir/mainwindow.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
