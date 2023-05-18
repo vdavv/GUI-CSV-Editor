846dbf6798fe4c65e8e9d406c6baca351c4dfdf4
vdavv <vdavvv@ya.ru>
2023-05-18 14:10:12 +0300

    first commit

---
+++ build-healthServices-Clang-Debug/CMakeFiles/healthServices.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/healthServices_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/healthServices_autogen.dir/ParseCache.txt"
+  "healthServices_autogen"
+  "CMakeFiles/healthServices.dir/healthServices_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/healthServices.dir/healthServices_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/healthServices.dir/main.cpp.o"
+  "CMakeFiles/healthServices.dir/main.cpp.o.d"
+  "CMakeFiles/healthServices.dir/mainwindow.cpp.o"
+  "CMakeFiles/healthServices.dir/mainwindow.cpp.o.d"
+  "healthServices.app/Contents/MacOS/healthServices"
+  "healthServices.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/healthServices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-healthServices-Clang-Debug/CMakeFiles/healthServices_autogen.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for healthServices_autogen.
+# This may be replaced when dependencies are built.
